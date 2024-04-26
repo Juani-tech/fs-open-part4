ea5274ee507e794e97d05b768e1c17bfc95fdaff
Juani-tech <jperezdichiazza@gmail.com>
2024-04-26 13:11:26 -0300

    part 4.8, all tests passing. Need to fix frontend

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,9 +1,11 @@
 // Esto es un middleware, fijarse que tiene que ver con el acortamiento de las rutas, por ejemplo.
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
+
   response.json(notes);
 });
 
@@ -19,12 +21,18 @@ notesRouter.get("/:id", async (request, response, next) => {
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
 
